7e7732dff3dd820e1bacf34e50f1dd8251df2ef9
grmBase <grmBase@users.noreply.github.com>
2022-03-19 18:19:05 +0000

    deploy: 0b264b953c988ed4ee3b6a7b40c8831b62775d8c

--- search/functions_2.js
+++ search/functions_2.js
@@ -1,5 +1,6 @@
 var searchData=
 [
-  ['client_5fconnect_0',['client_connect',['../classmrpc_1_1i__server.html#afc2201079104a626459a403c64663a53',1,'mrpc::i_server::client_connect()'],['../classmrpc_1_1t__server.html#a1aeadf60f95ff918feb81f839dccc1fd',1,'mrpc::t_server::client_connect()'],['../classt__appl.html#a3983ee9901e5f3dbef0b18ac4840e916',1,'t_appl::client_connect()']]],
-  ['create_1',['create',['../classmrpc_1_1i__server.html#ac27456bb7e3d43cba8f2f44b52bdd1fe',1,'mrpc::i_server']]]
+  ['clear_5fdrv_5frp_0',['clear_drv_rp',['../classmrpc_1_1i__driver.html#a0a63c750208301696c2c356731919ba1',1,'mrpc::i_driver::clear_drv_rp()'],['../classmrpc_1_1tcp__connect.html#a1cf8aed37a225913467c7d5bdc092ac4',1,'mrpc::tcp_connect::clear_drv_rp()']]],
+  ['client_5fconnect_1',['client_connect',['../classmrpc_1_1i__server.html#afc2201079104a626459a403c64663a53',1,'mrpc::i_server::client_connect()'],['../classmrpc_1_1t__server.html#a1aeadf60f95ff918feb81f839dccc1fd',1,'mrpc::t_server::client_connect()'],['../classt__appl.html#a3983ee9901e5f3dbef0b18ac4840e916',1,'t_appl::client_connect()']]],
+  ['create_2',['create',['../classmrpc_1_1i__server.html#ac27456bb7e3d43cba8f2f44b52bdd1fe',1,'mrpc::i_server']]]
 ];
--- search/functions_d.js
+++ search/functions_d.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['t_5fappl_0',['t_appl',['../classt__appl.html#ab581c92082feb9bd18013303631b4c7f',1,'t_appl']]],
   ['t_5fcmd_5frecord_1',['t_cmd_record',['../classmrpc_1_1t__cmd__record.html#a56177d8237e59475e8728a3980c0291f',1,'mrpc::t_cmd_record']]],
-  ['t_5fcustom_5fdrv_2',['t_custom_drv',['../classtst_1_1t__custom__drv.html#ae500033782e2ab4faef529de2ab7dd49',1,'tst::t_custom_drv']]],
+  ['t_5fcustom_5fdrv_2',['t_custom_drv',['../classtst_1_1t__custom__drv.html#a1ae83395d8a3d61af4643aba486f774d',1,'tst::t_custom_drv']]],
   ['t_5fdefer_5frec_3',['t_defer_rec',['../classmrpc_1_1t__defer__rec.html#a061089c87ff7c214fbbb04218c1ed7d9',1,'mrpc::t_defer_rec::t_defer_rec(std::future&lt; t_payload &gt; &amp;a_future)'],['../classmrpc_1_1t__defer__rec.html#a232273f6b2785adaecb6030d8ef2f067',1,'mrpc::t_defer_rec::t_defer_rec()']]],
   ['t_5fiocp_5fblock_4',['t_iocp_block',['../classt__iocp__block.html#aaa7f01f6902315728877d05991253f27',1,'t_iocp_block']]],
   ['t_5fserver_5',['t_server',['../classmrpc_1_1t__server.html#ada0b4830291c3ae2c659ff8ef3a7548d',1,'mrpc::t_server']]],
